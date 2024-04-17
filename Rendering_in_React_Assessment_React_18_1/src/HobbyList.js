402cdbeef23ef5db99796ca94a6dd1f82e97ac55
Kenneth <kennethyoung.2960@gmail.com>
2024-04-17 05:35:30 -0500

    Added ternary condition to HobbyList for no hobbies present condition

--- Rendering_in_React_Assessment_React_18_1/src/HobbyList.js
+++ Rendering_in_React_Assessment_React_18_1/src/HobbyList.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./HobbyList.css";
 
 function HobbyList({ hobbies }) {
-  return (
+  return hobbies.length === 0 ? null : (
     <div className="hobby-list">
       {/* vv You must use this heading for this component vv */}
       <h4>Hobbies</h4>;
