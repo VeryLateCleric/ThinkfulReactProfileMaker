55434a8ce1abbc18f34a8476341699e67145e7ba
Kenneth <kennethyoung.2960@gmail.com>
2024-04-17 04:56:06 -0500

    Header nearly finished

--- Rendering_in_React_Assessment_React_18_1/src/Header.js
+++ Rendering_in_React_Assessment_React_18_1/src/Header.js
@@ -2,7 +2,15 @@ import React from "react";
 import "./Header.css";
 
 function Header() {
-  return null;
+  return (
+    <div>
+      <img src={} alt="Profile" className="profile-picture" />
+      <div className="profile-details">
+        <h1>{name}</h1>
+        <p>Birthday: {birthday}</p>
+      </div>
+    </div>
+  );
 }
 
 export default Header;
